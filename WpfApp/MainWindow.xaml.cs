04898eef6a7bf3d50e3febae0a955e02e5806744
j_tohirun <ibnalisher1@gmail.com>
2024-04-21 14:27:23 +0500

    Update MainWindow.xaml.cs
    deleted comments

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace WpfApp
             InitializeComponent();
 //khbcbshksch
 //// added some comment
-/// khvchdv
+
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
